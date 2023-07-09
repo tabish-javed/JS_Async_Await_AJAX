7e18ad452d179eabd179192df7e667c9a891ef9a
TABISH <tabishjaved@hotmail.com>
2023-07-09 19:49:24 +0530

    initial state

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+// https://countries-api-836d.onrender.com/countries/
