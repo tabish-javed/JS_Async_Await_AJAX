e3f1c6285c5a8197b61d3ef25286d0c1f913afef
TABISH <tabishjaved@hotmail.com>
2023-07-10 13:41:23 +0530

    first AJEX call

--- script.js
+++ script.js
@@ -5,4 +5,38 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-// https://countries-api-836d.onrender.com/countries/
+/*
+const REST_Countries = 'https://restcountries.com/v3.1/all';
+// const URL = 'https://countries-api-836d.onrender.com/countries/'
+
+
+function getCountryData (country) {
+    const request = new XMLHttpRequest();
+    const url = `https://restcountries.com/v3.1/name/${country}?fullText=true`;
+
+    request.open('GET', url);
+    request.send();
+    request.addEventListener('load', function () {
+        const [data] = JSON.parse(this.responseText);
+        console.log(data);
+
+        const html = `
+        <article class="country">
+        <img class="country__img" src="${data.flags.svg}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} millions</p>
+            <p class="country__row"><span>🗣️</span>${Object.entries(data.languages)[0][1]}</p>
+            <p class="country__row"><span>💰</span>${Object.entries(Object.entries(data.currencies)[0][1])[1][1]}</p>
+        </div>
+        </article>
+    `;
+        countriesContainer.insertAdjacentHTML('beforeend', html);
+        countriesContainer.style.opacity = 1;
+    });
+}
+
+getCountryData('India')
+
+*/
