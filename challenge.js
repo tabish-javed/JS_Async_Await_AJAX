15c4cca58840b36b33a1e720077b75976850289a
TABISH <tabishjaved@hotmail.com>
2023-07-14 13:35:38 +0530

    API call task completed

--- challenge.js
+++ challenge.js
@@ -20,26 +20,58 @@ function renderCountry (data, className = '') {
     countriesContainer.style.opacity = 1;
 }
 
-
-// Construct URL and supply search parameters as object notation
-const apiEndPoint = new URL('https://geocode.maps.co/reverse');
-// Parameters in object form
-const parameters = {
-    lat: 52.508,
-    lon: 13.381
+function urlConstructor (url, pathName, parameters = '') {
+    // Construct URL and supply search parameters as object notation
+    const baseURL = new URL(url);
+    // Construct search parameters from object
+    const searchParams = new URLSearchParams(parameters);
+    // Add search parameters to URL
+    baseURL.pathname = pathName
+    baseURL.search = searchParams;
+    // Final URL
+    return baseURL.toString();
 }
-// Construct search parameters from object
-const searchParams = new URLSearchParams(parameters)
-// Add search parameters to URL
-apiEndPoint.search = searchParams
-// Final URL
-const reverseLocationURL = apiEndPoint.toString()
 
 
-fetch(reverseLocationURL)
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        console.log(data);
-    })
+function whereAmI (lat, lon) {
+    const parameters = {
+        lat: lat,
+        lon: lon
+    };
+    const url = urlConstructor('https://geocode.maps.co', '/reverse', parameters);
+
+    fetch(url)
+        .then(response => {
+            console.log(response)
+            if (!response.ok) throw new Error('Invalid URL')
+            return response.json();
+        })
+        .then(data => {
+            const { city, country } = data.address;
+            console.log(`You are in ${city}, ${country}`);
+            const url2 = `https://restcountries.com/v3.1/name/${country}?fullText=true`
+
+            return fetch(url2)
+        })
+        .then(response => {
+            console.log(response)
+            return response.json()
+        })
+        .then(([data]) => {
+            renderCountry(data)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+
+// const latitude = 52.508;
+// const longitude = 13.381;
+
+// const latitude = 19.037;
+// const longitude = 72.873;
+
+const latitude = -33.933;
+const longitude = 18.474;
+
+whereAmI(latitude, longitude);
